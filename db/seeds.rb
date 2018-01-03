f4e9459deb5ab2a1419d2698a8562a1c71b32126
TMad <madsen.tyler@gmail.com>
2018-01-03 16:22:09 -0700

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.destroy_all
+Dog.destroy_all
+
+DOGS = ["Fido", "Bubbles", "Fluffy", "Rover", "Max", "Wolf", "Jim", "Captain", "Cooper", "Rocky", "Bear", "Duke"]
+CATEGORIES = ["Sporting Dogs", "Hounds", "Working Dogs", "Terriers", "Toys", "Non-Sporting", "Herding"]
+BREED = ["German Shepard", "Labrador", "Beagle", "Golden Retriever", "Poodle", "Pug"]
+SIZE = ["Small", "Medium", "Large"]
+CAT_FRIENDLY = ["True", "False"]
+GENDER = ["MALE", "FEMALE"]
+
+CATEGORIES.each do |title|
+  Category.create!(title: title)
+end
+DOGS.each do |name|
+  dog = Dog.create!(name: name, breed: BREED.sample, size: SIZE.sample, weight: rand(150), cat_friendly: CAT_FRIENDLY.sample, gender: GENDER.sample, description: "I LOVE HUMANS!", price: rand(200), category_id: Category.all.sample.id )
+  puts "Created #{dog.name}"
+end
