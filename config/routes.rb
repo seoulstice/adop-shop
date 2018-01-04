1cf35d6d2cef99855dd12e69f1d80e247732c0f4
seoulstice <jungyoungjoon@gmail.com>
2018-01-04 14:59:11 -0700

    custom routes for dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -24,5 +24,6 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+  get "/dashboard", to: "users#show"
 
 end
