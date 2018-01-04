4e7b2dfab3faaddf9b6ab0a952292202a92e0fe6
seoulstice <jungyoungjoon@gmail.com>
2018-01-04 15:36:42 -0700

    Added index route

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,5 +4,9 @@ class CategoriesController < ApplicationController
     @category = Category.find_by(slug: params[:id])
   end
 
+  def index
+    @categories = Category.order(:title)
+  end
+
 
 end
