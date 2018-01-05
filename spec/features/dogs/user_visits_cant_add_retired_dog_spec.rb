22ccad8a59d8c80ddde93879ffefc5e880f6e4ec
TMad <madsen.tyler@gmail.com>
2018-01-05 16:44:07 -0700

    add retired messaging to dog show + feature test

--- spec/features/dogs/user_visits_cant_add_retired_dog_spec.rb
+++ spec/features/dogs/user_visits_cant_add_retired_dog_spec.rb
@@ -4,12 +4,12 @@ describe "a user goes to the dog show page of a retired dog" do
   context "they do not see a button to add the dog to their cart" do
     it "the user sees item retired messaging in it's place" do
       category = create(:category)
-      dog = create(:dog, category: category)
+      dog = create(:dog, retired: true, category: category)
 
-      visit dog_path(dog)
+      visit dog_path(dog.slug)
 
       expect(page).to_not have_button("Add Dog")
-      expect(page).to have_content("Dog Retired")
+      expect(page).to have_link("Dog Retired. View Other Dogs!")
     end
   end
 end
