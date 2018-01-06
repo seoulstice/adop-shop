985d3e09d5489f9d762a8f27793d92b05b42be96
seoulstice <jungyoungjoon@gmail.com>
2018-01-06 13:11:22 -0700

    created user view

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   namespace :admin, only: [:new] do
     resources :dogs
     resources :carrierwave_images
+    get "/dashboard", to: "users#show"
   end
 
   resources :users, only: [:new, :create]
