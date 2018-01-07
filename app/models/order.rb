15a7e8f3b5ca32dbb43f12042f69fa86e90c7db8
seoulstice <jungyoungjoon@gmail.com>
2018-01-07 12:53:43 -0700

    Story 24 complete

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,18 +1,22 @@
 class Order < ApplicationRecord
   belongs_to :user
-  has_many :order_dogs 
-  has_many :dogs, through: :order_dogs 
+  has_many :order_dogs
+  has_many :dogs, through: :order_dogs
   enum status: ["Ordered", "Paid", "Canceled", "Completed"]
 
-  
 
-  def readable_date 
-    created_at.strftime("%D") 
+
+  def readable_date
+    created_at.strftime("%D")
   end
 
   def completed?
     return false if status == "Ordered" || status == "Paid"
   end
 
-  
+  def self.count_status
+    group(:status).count
+  end
+
+
 end
