4b9a9a25de38728672c4d509d117364cc9192451
seoulstice <jungyoungjoon@gmail.com>
2018-01-07 11:41:48 -0700

    test for story 24 passing

--- spec/features/admin/admin_can_edit_dog_spec.rb
+++ spec/features/admin/admin_can_edit_dog_spec.rb
@@ -5,19 +5,34 @@ describe "When an Admin User navigates to dogs index" do
     it "the Admin can edit the dog" do
       admin = create(:user, role: 1)
       category = create(:category)
-      dog = create(:dog, category: category)
+      dog = create(:dog, category: category, weight: 100)
       dog2 = create(:dog, category: category)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
-      visit 
+      visit admin_dogs_path
+
+      click_link("Edit #{dog.name}")
+
+      expect(current_path).to eq(edit_admin_dog_path(dog.slug))
+      page.should have_field("Name", with: "#{dog.name}")
+      page.should have_field("Breed", with: "#{dog.breed}")
+      expect(page).to have_select("dog_size", with_options: ["Small", "Medium", "Large"])
+      page.should have_field("Weight", with: "#{dog.weight}")
+      # expect(page).to have_checked_field("dog[cat_friendly]")
+      expect(page).to have_select("dog_gender", with_options: ["Male", "Female"])
+      page.should have_field("Description", with: "#{dog.description}")
+      expect(page).to have_select("dog_category_id", with_options: ["#{category.title}"])
+      expect(page).to have_unchecked_field("dog[retired]")
+
+      fill_in "dog[weight]", with: 200
+      click_button "Update Dog"
+
+      expect(current_path).to eq(admin_dogs_path)
+
+      visit dog_path(dog.slug)
+
+      expect(page).to have_content("200")
     end
   end
 end
-
-
-As an admin
-When I visit “admin/items”
-And I click “Edit”
-Then my current path should be “/admin/items/:ITEM_ID/edit”
-And I should be able to update title, description, image, and status
