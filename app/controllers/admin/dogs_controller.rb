490814d38410dd58d807ad95f683653e43d412a0
nico24687 <theonlynico@hotmail.com>
2018-01-08 14:07:36 -0700

    only able to save dogs to the database that have a unique name

--- app/controllers/admin/dogs_controller.rb
+++ app/controllers/admin/dogs_controller.rb
@@ -11,14 +11,14 @@ class Admin::DogsController < Admin::BaseController
 
   def create
     @dog = Dog.new(dog_params)
-    if @dog.save!
+    if @dog.save
       params[:dog][:category_ids].drop(1).each do |category_id|
         @dog.dog_categories.create(category_id: category_id)
       end
       flash[:success] = "You've created a new dog."
       redirect_to dog_path(@dog.slug)
     else
-      flash[:failure] = "Try again."
+      flash[:failure] = "Try again, please enter the correct info."
       render :new
     end
   end
