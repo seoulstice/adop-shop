19f51966fc3e1d78e01a29ce79056dc8830f366b
seoulstice <jungyoungjoon@gmail.com>
2018-01-09 13:50:40 -0700

    Test for story 28

--- spec/features/admin/admin_can_see_analytics_breakdowns_spec.rb
+++ spec/features/admin/admin_can_see_analytics_breakdowns_spec.rb
@@ -20,20 +20,33 @@ describe "When an Admin User navigates to analytics dashboard" do
     admin = create(:user, role: 1)
     user1 = create(:user)
     user2 = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
     dog1 = create(:dog, price: 50)
     dog2 = create(:dog, price: 100)
     dog3 = create(:dog, price: 150)
+    dog4 = create(:dog, price: 100)
+
     category1 = create(:category, title: "Working Dog")
     category2 = create(:category, title: "Toy")
     dog_category1 = create(:dog_category, dog: dog1, category: category1)
-    dog_category2 = create(:dog_category, dog: dog2, category: category2)
+    dog_category2 = create(:dog_category, dog: dog2, category: category1)
     dog_category3 = create(:dog_category, dog: dog3, category: category2)
+    dog_category4 = create(:dog_category, dog: dog4, category: category2)
+
     order1 = create(:order, user: user1)
     order2 = create(:order, user: user2)
+
     order_dog1 = create(:order_dog, dog: dog1, order: order1)
     order_dog2 = create(:order_dog, dog: dog2, order: order2)
     order_dog3 = create(:order_dog, dog: dog3, order: order2)
 
-    expect(page).to have_content("Most Expensive Working Dog : #{dog}")
+    visit admin_analytics_dashboard_path
+
+    expect(page).to have_content("Most Expensive Working Dog: #{dog2.name}")
+    expect(page).to have_content("Most Expensive Toy: #{dog3.name}")
+
+    expect(page).to have_content("Toy: 1")
+    expect(page).to have_content("Working Dog: 2")
   end
 end
