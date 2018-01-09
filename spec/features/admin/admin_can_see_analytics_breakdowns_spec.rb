0ebae3a13bb5726abb786d8da59c57eb59221603
seoulstice <jungyoungjoon@gmail.com>
2018-01-09 15:09:33 -0700

    Spec Story 29 passing

--- spec/features/admin/admin_can_see_analytics_breakdowns_spec.rb
+++ spec/features/admin/admin_can_see_analytics_breakdowns_spec.rb
@@ -53,24 +53,12 @@ describe "When an Admin User navigates to analytics dashboard" do
   it "the Admin can see Retired Item analytics" do
     admin = create(:user, role: 1)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-    dog1 = create(:dog, price: 50)
-    dog2 = create(:dog, price: 100)
+    dog1 = create(:dog, price: 50, retired_count: 2)
+    dog2 = create(:dog, price: 100, retired_count: 1)
     dog3 = create(:dog, price: 75)
 
-    visit admin_dogs_path
-
-    page.all("input[type='checkbox']")[0].click
-    page.all("input[type='submit']")[0].click
-    page.all("input[type='checkbox']")[0].click
-    page.all("input[type='submit']")[0].click
-    page.all("input[type='checkbox']")[0].click
-    page.all("input[type='submit']")[0].click
-
-    page.all("input[type='checkbox']")[1].click
-    page.all("input[type='submit']")[1].click
-
     visit admin_analytics_dashboard_path
-    binding.pry
+
     expect(page).to have_content("#{dog1.name}")
     expect(page).to have_content("#{dog1.retired}")
     expect(page).to have_content("2")
