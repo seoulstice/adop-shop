03294892ffb06fbc749936762ba0f8c1d8707253
nico24687 <theonlynico@hotmail.com>
2018-01-10 15:19:42 -0700

    fixed test user_can_login

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,13 @@
 FactoryBot.define do
-  factory :user do
-    password "password"
-    sequence(:username) {|n| "username#{n}"}
-    role 0
-    address "address"
-    sequence(:name) {|n| "Unique#{n}"}
-    orders_count 0
-  end
+ factory :user do
+   password "password"
+   sequence(:username) {|n| "username#{n}"}
+   role 0
+   address "address"
+   city "city"
+   state "CO"
+   zipcode 12345
+   sequence(:name) {|n| "Unique#{n}"}
+   orders_count 0
+ end
 end
