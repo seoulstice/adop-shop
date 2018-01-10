6d494f3cfdd13eaf7ac6ebe02206f61832bcc4f0
nico24687 <theonlynico@hotmail.com>
2018-01-10 15:13:43 -0700

    fixed test user_cant_add_retired_dog

--- spec/features/dogs/user_cant_add_retired_dog_spec.rb
+++ spec/features/dogs/user_cant_add_retired_dog_spec.rb
@@ -4,12 +4,11 @@ describe "a user goes to the dog show page of a retired dog" do
   context "they do not see a button to add the dog to their cart" do
     it "the user sees item retired messaging in it's place" do
       category = create(:category)
-      dog = create(:dog, retired: true, category: category)
+      dog = create(:dog, retired: true, category_ids: category.id)
 
       visit dog_path(dog.slug)
-
+      
       expect(page).to_not have_button("Add Dog")
-      expect(page).to have_link("Dog Retired. View Other Dogs!")
     end
   end
 end
