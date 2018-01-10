72ae8668ffc40c0391a483d7d39bf735e0fdfe46
nico24687 <theonlynico@hotmail.com>
2018-01-10 15:07:48 -0700

    fixed test user_can_add_dogs_to_cart

--- spec/features/dogs/user_can_add_dogs_to_cart_spec.rb
+++ spec/features/dogs/user_can_add_dogs_to_cart_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 feature "when a user adds a dog to their cart" do
   before(:each) do
     @category = create(:category)
-    @dog = create(:dog, category_id: @category.id)
+    @dog = create(:dog, category_ids: @category.id)
+    
   end
 
   scenario "a message is displayed" do
@@ -17,12 +18,12 @@ feature "when a user adds a dog to their cart" do
   scenario "the message correctly increments for multiple dogs" do
     visit dogs_path
 
-    expect(page).to have_content("Cart: 0")
+    expect(page).to have_content("Cart (0)")
 
     click_button "Add Dog"
 
     expect(page).to have_content("You now have 1 #{@dog.name} in your cart")
-    expect(page).to have_content("Cart: 1")
+    expect(page).to have_content("Cart (1)")
     
     click_button "Add Dog"
 
